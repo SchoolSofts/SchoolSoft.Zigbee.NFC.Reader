47ab071863617160e37d33ab228d5d424a5ac262
Nazar Kacharaba <nazarr.kacharaba@gmail.com>
2024-03-11 22:35:15 +0200

    Update NFCReader.ino

--- NFCReader/NFCReader.ino
+++ NFCReader/NFCReader.ino
@@ -6,12 +6,16 @@
 PN532_I2C pn532_i2c(Wire);
 NfcAdapter nfc = NfcAdapter(pn532_i2c);
 
+SoftwareSerial zigBeeSerial(2, 3);
+
 String tagId = "None";
 byte nuidPICC[4];
 
 void setup(void)
 {
   Serial.begin(115200);
+  zigBeeSerial.begin(9600);
+  
   Serial.println("System initialized");
   nfc.begin();
 }
@@ -28,8 +32,11 @@ void readNFC()
     NfcTag tag = nfc.read();
     tag.print();
     tagId = tag.getUidString();
+    
     digitalWrite(13, HIGH);
     delay(1000);
     digitalWrite(13, LOW);
+    
+    zigBeeSerial.println(tagId);
   }
 }
